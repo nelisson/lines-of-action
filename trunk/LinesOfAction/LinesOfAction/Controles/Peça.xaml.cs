a600ed5263c61471ba3026a6f1ba0d0f9e976d3d
nelisson.ad@gmail.com <nelisson.ad@gmail.com@b34d17b0-e78a-4ee9-9e26-4a7ea3e47c2b>
2012-02-24 14:25:19 +0000

    Tabuleiro Pronto, falta opções na lateral

---
+++ trunk/LinesOfAction/LinesOfAction/Controles/Peça.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using LinesOfAction.Enumerações;
+
+namespace LinesOfAction.Controles
+{
+    /// <summary>
+    ///   Interaction logic for Peça.xaml
+    /// </summary>
+    public partial class Peça : INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty JogadorProperty =
+            DependencyProperty.Register("Jogador", typeof (Jogadores), typeof (Peça),
+                                        new UIPropertyMetadata(Jogadores.Jogador1));
+
+        private int _coluna;
+
+        private int _linha;
+
+        public Peça()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        public int Linha
+        {
+            get { return _linha; }
+            set
+            {
+                _linha = value;
+                NotifyPropertyChanged("Linha");
+            }
+        }
+
+        public int Coluna
+        {
+            get { return _coluna; }
+            set
+            {
+                _coluna = value;
+                NotifyPropertyChanged("Coluna");
+            }
+        }
+
+        public Jogadores Jogador
+        {
+            get { return (Jogadores) GetValue(JogadorProperty); }
+            set { SetValue(JogadorProperty, value); }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            DataContext = this;
+        }
+
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/LinesOfAction/LinesOfAction/Enumerações/Jogadores.cs
@@ -0,0 +1,8 @@
+﻿namespace LinesOfAction.Enumerações
+{
+    public enum Jogadores
+    {
+        Jogador1,
+        Jogador2,
+    }
+}
